53bc12339f89b4a379bcddd4f8891e49889156bc
Shubham Pal <shubpal@google.com>
2026-01-28 10:28:35 +0000

    Migrate configure_kueue from gavinbunney to helm
    Change-Id: I7b582a525f41af8124d45cda9277188fb9446f3d

--- modules/management/kubectl-apply/versions.tf
+++ modules/management/kubectl-apply/versions.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2026 Google LLC
+ * Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,10 @@ terraform {
     http = {
       source  = "hashicorp/http"
       version = "~> 3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
     }
   }
 
